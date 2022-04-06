69967f422197eae5f24cbf0993104c219fa0a43e
windiewhale <feketebodza@gmail.com>
2022-04-06 09:37:59 +0200

    customer data send to file

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,17 @@ const path = require("path")
 
 const app = express();
 const port = 9000
+const fFolder = `${__dirname}/../frontend`
+const userFile = path.join(`${__dirname}/../frontend/users.json`)
+
+app.use(express.json())
 
 app.get('/', (req, res, next) => {
-    res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
+    res.sendFile(path.join(`${fFolder}/index.html`));
+})
+
+app.get('/admin/order-view', (req, res, next) => {
+    res.sendFile(path.join(`${fFolder}/index.html`));
 })
 
 app.get('/kismacska', (req, res, next) => {
@@ -18,41 +26,76 @@ app.get('/something', (request, response, next) => {
     response.send("thank you for your request, this is our response for something endpoint")
 })
 
-const userFile = path.join(`${__dirname}/../frontend/users.json`)
-
-app.get('/api/v1/users/active', (request, response, next) => {
-    fs.readFile(userFile, (error, data) => {
-        if (error) {
-            response.send(error)
-        } else {
-            const users = JSON.parse(data)
-            const activeUsers = users.filter(user => user.status === "active")
-            response.send(activeUsers)
-        }
-    })
-});
-
-app.get('/api/v1/users/passive', (request, response, next) => {
-    fs.readFile(userFile, (error, data) => {
-        if (error) {
-            response.send("error happened")
-        } else {
-            const users = JSON.parse(data)
-            const passiveUsers = users.filter(user => user.status === "passive")
-            response.send(passiveUsers)
-        }
-    })
-})
 
 app.get('/api/v1/users', (request, response, next) => {
     console.log("request recieved on users endpoint");
     response.sendFile(userFile);
 })
 
-app.use('/pub', express.static(`${__dirname}/../frontend/public`)); //statikus mappa kiszolgálás, ez úgy ahogy van látható a servernek. nem szerencsés betenni az indexet ide.
+app.get('/api/v1/users-query', (request, response, next) => {
+    console.dir(request.query)
+    console.log(request.query.apiKey)
+    if (request.query.apiKey === "apple") {
+        response.sendFile(userFile);
+    } else {
+        response.send("unauthorized request")
+    }
+})
+
+app.get('/api/v1/users-params/:key', (req, res, next) => {
+    console.dir(req.params)
+    console.log(req.params.key)
+    if (req.params.key === "apple") {
+        res.send("you typed APPLE")
+    } else {
+        res.send("you did not type APPLE!")
+    }
+})
+
+app.get('/api/v1/users-params/:status', (request, response, next) => {
+    fs.readFile(userFile, (error, data) => {
+        const users = JSON.parse(data)
+        if (error) {
+            request.send("problema")
+        } else {
+        
+            if (request.params.status === "active") {
+                const activeUsers = users.filter(user => user.status === "active")
+                response.send(activeUsers)
+            } else if (request.params.status === "passive") {
+                const passiveUsers = users.filter(user => user.status === "passive")
+                response.send(passiveUsers)
+            } else {
+                response.send(error)
+            }
+        }
+    })
+});
+
+app.post("/users/new", (req, res) => {
+    fs.readFile(userFile, (error, data) => {
+        if (error) {
+            console.log(error);
+            res.send("un poco problema al leer de archivo de usuarios")
+        } else {
+            const users = JSON.parse(data);
+            console.log(req.body);
+            users.push(req.body);
+
+            fs.writeFile(userFile, JSON.stringify(users), error => {
+                if (error) {
+                    console.log(error);
+                    res.send("problema al escribir el archivo useres")
+                }
+            })
+            res.send(req.body)
+        }
+    })
+})
 
 app.listen(port, () => {
     console.log(`http://127.0.0.1:${port}`);
 })
 
+app.use('/pub', express.static(`${__dirname}/../frontend/public`)); 
 
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -12,7 +12,24 @@ const userComponent = ({name, surname}) => {
     `
 } 
 
+function addUserComponent() {
+    return `
+        <div>
+            <input type="text name="firstName" class="firstname" placeholder="First Name">
+            <input type="text name="surname" class="surname" placeholder="Surname">
+            <button id="btn">Send</button>
+        </div>
+    `
+}
+
 const loadEvent = async () => {
+
+    //window.location a web api része. 
+    if (window.location.pathname === '/admin/order-view') {
+        console.log("you are at the admin site");
+    } else {
+        console.log("you are at the customer site");
+    }
 
     const result = await parseJSON("/api/v1/users")
     const rootElement = document.getElementById("root")
@@ -21,7 +38,32 @@ const loadEvent = async () => {
         "beforeend", 
         result.map(user => userComponent(user)).join("")
     );
+    rootElement.insertAdjacentHTML("afterend", addUserComponent())
+    
+    const button = document.getElementById("btn")
+    const firstName = document.querySelector(".firstname")
+    const surname = document.querySelector(".surname")
+    
+    button.addEventListener("click", () => {
+        const newUserData = {
+            name: firstName.value,
+            surname: surname.value,
+            status: "active"
+        }
 
+        fetch ("/users/new", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newUserData)
+        })
+        .then(async data => {
+            const user = await data.json();
+
+            rootElement.insertAdjacentHTML("beforeend", userComponent(user))
+        })    
+    })
 }
 
 window.addEventListener("load", loadEvent)
